4c75f9c2362ed25382070875387acc1290b48b42
Luis Roman <porta5648@gmail.com>
2021-02-01 16:11:32 -0600

    another project added

--- js/slides.js
+++ js/slides.js
@@ -1,5 +1,7 @@
 var slideIndex = 0;
+var slideIndexGi = 0;
 showSlides();
+showSlidesGi();
 
 function showSlides() {
   var i;
@@ -11,4 +13,16 @@ function showSlides() {
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
+
+function showSlidesGi() {
+  var i;
+  var slides = document.getElementsByClassName("slideGi");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndexGi++;
+  if (slideIndexGi > slides.length) {slideIndexGi = 1}
+  slides[slideIndexGi-1].style.display = "block";
+  setTimeout(showSlidesGi, 5000); // Change image every 4 seconds
 }
