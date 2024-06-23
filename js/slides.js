61a8ef0eb5801d12260992219e026251f0046561
Luis Roman <porta5648@gmail.com>
2024-06-22 18:11:50 -0600

    portfolio updated

--- js/slides.js
+++ js/slides.js
@@ -14,6 +14,18 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
+function showSlidesGapp() {
+  var i;
+  var slides = document.getElementsByClassName("slideGapp");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 4000); // Change image every 4 seconds
+}
+
 
 function showSlidesGi() {
   var i;
--- js/text.js
+++ js/text.js
@@ -13,3 +13,18 @@ function readM() {
     moreText.style.display = "inline";
   }
 }
+function readMGapp() {
+  var dots = document.getElementById("dotsGapp");
+  var moreText = document.getElementById("moreGapp");
+  var btnText = document.getElementById("btnTxtGapp");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
